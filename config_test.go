6630b1828a1f70db4665ed972ed56d78ddb6988a
Hugo Correia <hfcorreia91@gmail.com>
2018-12-17 15:48:32 +0000

    Add copy and dynamodb skeletons

---
+++ config_test.go
@@ -0,0 +1,26 @@
+package dynamodbcopy_test
+
+import (
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/uniplaces/dynamodbcopy"
+	"testing"
+)
+
+func TestNewConfig(t *testing.T) {
+	viperConfig := viper.New()
+
+	expectedConfig := dynamodbcopy.Config{
+		TargetTable: "test-Target",
+		SourceTable: "test-Source",
+	}
+
+	viperConfig.Set("source-table", expectedConfig.SourceTable)
+	viperConfig.Set("target-table", expectedConfig.TargetTable)
+
+	config, err := dynamodbcopy.NewConfig(*viperConfig)
+	require.Nil(t, err)
+
+	assert.Equal(t, expectedConfig, config)
+}
--- pkg/cmd/copytable/copytable.go
+++ pkg/cmd/copytable/copytable.go
@@ -2,9 +2,10 @@ package copytable
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/uniplaces/dynamodbcopy"
+	"log"
 )
 
 const (
@@ -18,16 +19,50 @@ func New(config *viper.Viper) *cobra.Command {
 		Short: shortDescription,
 		Args:  cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
+			config.SetDefault("source-table", args[0])
+			config.SetDefault("target-table", args[1])
+
+			service, err := dynamodbcopy.NewDynamoDBCopy(*config)
+			if err != nil {
+				log.Fatalf("%s error: %s", cmdName, err)
+			}
+
+			if err := Run(service); err != nil {
+				log.Fatalf("%s error: %s", cmdName, err)
+			}
 		},
 	}
 
-	cmd.Flags().StringP("source-region", "s", "eu-west-1", "Set the AWS region for the source table")
-	cmd.Flags().StringP("target-region", "t", "eu-west-1", "Set the AWS region for the target table")
-	cmd.Flags().IntP("read-provision", "r", 0, "Set the read provision to set for the source table")
-	cmd.Flags().IntP("write-provision", "w", 0, "Set the write provision to set for the target table")
-	if err := config.BindPFlags(cmd.Flags()); err != nil {
+	if err := SetAndBindFlags(cmd, config); err != nil {
 		panic(err)
 	}
 
 	return cmd
+}
+
+func SetAndBindFlags(cmd *cobra.Command, config *viper.Viper) error {
+	cmd.Flags().StringP("source-profile", "s", "", "Set the profile to use for the source table")
+	cmd.Flags().StringP("target-profile", "t", "", "Set the profile to use for the target table")
+	cmd.Flags().IntP("read-units", "r", 0, "Set the read provisioned capacity for the source table")
+	cmd.Flags().IntP("write-units", "w", 0, "Set the write provisioned capacity for the target table")
+
+	return config.BindPFlags(cmd.Flags())
+}
+
+func Run(service dynamodbcopy.DynamoDBCopy) error {
+	initialProvision, err := service.FetchProvisioning()
+	if err != nil {
+		return err
+	}
+
+	copyProvision := service.CalculateCopyProvisioning(initialProvision)
+	if err := service.UpdateProvisioning(copyProvision); err != nil {
+		return err
+	}
+
+	if err := service.Copy(); err != nil {
+		return err
+	}
+
+	return service.UpdateProvisioning(initialProvision)
 }
