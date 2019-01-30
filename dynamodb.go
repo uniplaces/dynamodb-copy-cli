f4178f451233c3fabc0a86b3e33b8faa0a4eaf2e
João Almeida <jmdalmeida@users.noreply.github.com>
2019-01-30 12:59:25 +0000

    Update dynamodb.go
    Co-Authored-By: hfcorreia <hfcorreia91@gmail.com>

--- dynamodb.go
+++ dynamodb.go
@@ -155,7 +155,7 @@ func (db dynamoDBSerivce) batchWriteItem(requests []*dynamodb.WriteRequest) erro
 					db.logger.Printf("batch write provisioning error: waited %d ms (attempt %d)", elapsed, attempt)
 					return false, nil
 				case errCodeThrottlingException:
-					db.logger.Printf("batch write throttling error: waited %d ms (attempt %d) ", elapsed, attempt)
+					db.logger.Printf("batch write throttling error: waited %d ms (attempt %d)", elapsed, attempt)
 					return false, nil
 				default:
 					return false, fmt.Errorf(
